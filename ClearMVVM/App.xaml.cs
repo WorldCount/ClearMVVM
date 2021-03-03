99b9fa3999f9365cdb0beb70e600405145b4e033
WorldCount <World.Count@yandex.ru>
2021-03-03 12:27:17 +0300

    Рефакторинг.

--- ClearMVVM/App.xaml.cs
+++ ClearMVVM/App.xaml.cs
@@ -4,41 +4,43 @@ using System.Windows;
 using ClearMVVM.Services;
 using ClearMVVM.ViewModels;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace ClearMVVM
 {
     public partial class App
     {
+        private static IHost _host;
+
+        #region Свойства
         public static Window ActivedWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsActive);
 
         public static Window FocusedWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsFocused);
 
-        private static IHost _host;
-
-        public static IHost Host => _host ??= Microsoft.Extensions.Hosting.Host
-            .CreateDefaultBuilder(Environment.GetCommandLineArgs())
+        public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs())
             .ConfigureAppConfiguration(cfg => cfg.AddJsonFile("app_settings.json", true, true))
-            .ConfigureServices((host, services) => services
-                .AddViews()
-                .AddServices()
-            )
             .Build();
 
         public static IServiceProvider Services => Host.Services;
+
+        #endregion
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services) => services
+            .AddServices()
+            .AddViewModels();
 
         protected override async void OnStartup(StartupEventArgs e)
         {
             var host = Host;
             base.OnStartup(e);
-            await host.StartAsync();
+            await host.StartAsync().ConfigureAwait(false);
         }
 
         protected override async void OnExit(ExitEventArgs e)
         {
             base.OnExit(e);
-            using var host = Host;
-            await host.StopAsync();
+            using (Host) await Host.StopAsync().ConfigureAwait(false);
         }
     }
 }
--- ClearMVVM/ViewModels/ViewModelRegistrator.cs
+++ ClearMVVM/ViewModels/ViewModelRegistrator.cs
@@ -4,7 +4,7 @@ namespace ClearMVVM.ViewModels
 {
     internal static class ViewModelRegistrator
     {
-        public static IServiceCollection AddViews(this IServiceCollection services) => services
+        public static IServiceCollection AddViewModels(this IServiceCollection services) => services
             .AddSingleton<MainWindowViewModel>();
     }
 }
